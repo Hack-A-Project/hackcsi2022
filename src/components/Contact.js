8bb28b117cba3a51bb50ccecc46e0ed082a16612
prince-ao <princead@tutanota.com>
2021-12-28 17:27:31 -0500

    general styling & contact improvement

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,55 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import "./stylesheets/Contact.css";
 import { Typography } from "@mui/material";
 
+function getWindowDimensions() {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+		width,
+		height,
+	};
+}
+
 export default function Contact({ isOn }) {
-  const [state, handleSubmit] = useForm("mayazako");
-  if (state.succeeded) {
-    return (
-      <div style={{ color: "white", textAlign: "center" }} className="contact">
-        <p>We will get back to you as soon as possible!</p>
-      </div>
-    );
-  }
-  return (
-    <div
-      className="contact"
-      style={{
-        color: isOn ? "black" : "#f3f3f3",
-      }}
-    >
-      <form
-        className="form"
-        onSubmit={handleSubmit}
-        style={{
-          backgroundColor: isOn ? "white" : "#181818",
-        }}
-      >
-        <Typography className="contact__title" variant="h4">
-          Got A Question?
-        </Typography>
-        <label htmlFor="name">First Name & Last Name</label>
-        <input id="name" type="text" name="name" />
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <br />
-        <button
-          className="submit__button"
-          type="submit"
-          disabled={state.submitting}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+	const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+	const [state, handleSubmit] = useForm("mayazako");
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimensions(getWindowDimensions());
+		}
+
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+	if (state.succeeded) {
+		return (
+			<div style={{ color: "white", textAlign: "center" }} className="contact">
+				<p>We will get back to you as soon as possible!</p>
+			</div>
+		);
+	}
+	return (
+		<div
+			className="contact"
+			style={{
+				color: isOn ? "black" : "#f3f3f3",
+			}}
+		>
+			<form
+				className="form"
+				onSubmit={handleSubmit}
+				style={{
+					backgroundColor: isOn ? "white" : "#181818",
+				}}
+			>
+				<Typography className="contact__title" variant={windowDimensions.width < 600 ? "h5" : "h4"}>
+					Got A Question?
+				</Typography>
+				<label className="name__label" htmlFor="name">
+					First Name & Last Name
+				</label>
+				<input id="name" type="text" name="name" />
+				<label className="email__label" htmlFor="email">
+					Email Address
+				</label>
+				<input id="email" type="email" name="email" />
+				<ValidationError prefix="Email" field="email" errors={state.errors} />
+				<label className="message__label" htmlFor="message">
+					Message
+				</label>
+				<textarea id="message" name="message" />
+				<ValidationError prefix="Message" field="message" errors={state.errors} />
+				<br />
+				<button className="submit__button" type="submit" disabled={state.submitting}>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
